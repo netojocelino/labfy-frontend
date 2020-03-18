224f81a495b9e73309149b767e74de249532d387
Neto Jocelino <netojocelino@gmail.com>
2020-03-17 21:23:45 -0300

    Remodelação de Card do quadro

---
+++ src/page/Card/index.js
@@ -0,0 +1,39 @@
+import React, { useContext } from 'react';
+
+import { MdArchive, MdKeyboardArrowRight } from 'react-icons/md';
+
+import CardStyle from './styles'
+
+import BoardContext from '../../context';
+
+export default function Card ({ data }) {
+
+    const { move } = React.useContext(BoardContext);
+
+    const state = {
+        todo: '',
+        doing: '',
+        done: ''
+    };
+    
+
+    return (
+    <CardStyle.Container name="Card">
+        <header>
+            <CardStyle.Label key={data.label} color={data.label} />
+            { data.list_state.toLowerCase() !== 'done' && data.list_state.toLowerCase() !== 'fridge' && 
+                <CardStyle.Icon onClick={ () => move(data.card_id, 'NexT') } title={data.action.title}>
+                <MdKeyboardArrowRight size={17} color="#ccc" />
+            </CardStyle.Icon>}
+            <CardStyle.Icon onClick={ () => move(data.card_id, "delete") }
+                title={data.list_state.toLowerCase() === 'fridge' ? 'desarquivar' : 'Arquivar'} right={26}>
+                <MdArchive size={17} color="#ccc" />
+            </CardStyle.Icon>
+        </header>
+        <h4>{data.title}</h4>
+        <p>{ data.content }</p>
+        <CardStyle.Owner>Solicitado por { data.created_by}</CardStyle.Owner>
+        {data.user && <img src={ data.user } alt="" />}
+    </CardStyle.Container>
+    );
+}
